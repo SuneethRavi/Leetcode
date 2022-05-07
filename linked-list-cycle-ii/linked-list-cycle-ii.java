16dae5a7c7508895a05ac37cac9b9e6332d1b811
SuneethRavi <30765739+SuneethRavi@users.noreply.github.com>
2022-05-07 15:52:45 -0400

    Time: 0 ms, Memory: 45.5 MB - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.java
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,16 +1,35 @@
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        Set<ListNode> visited = new HashSet<ListNode>();
-
-        ListNode node = head;
-        while (node != null) {
-            if (visited.contains(node)) {
-                return node;
+    private ListNode getIntersect(ListNode head) {
+        ListNode tortoise = head;
+        ListNode hare = head;
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+            if (tortoise == hare) {
+                return tortoise;
             }
-            visited.add(node);
-            node = node.next;
         }
 
         return null;
+}
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+     
+        ListNode intersect = getIntersect(head);
+        if (intersect == null) {
+            return null;
+        }
+
+        ListNode ptr1 = head;
+        ListNode ptr2 = intersect;
+        while (ptr1 != ptr2) {
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+
+        return ptr1;
     }
 }
